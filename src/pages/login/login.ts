f5aae53ae7d9168ed5ab7d58710af5fff5d4e19b
sacter <741170471@qq.com>
2018-06-04 23:53:25 +0800

    'login'

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the LoginPage page.
@@ -18,8 +19,16 @@ export class LoginPage {
     constructor(public navCtrl: NavController, public navParams: NavParams) {
     }
 
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad LoginPage');
+    logIn(username: HTMLInputElement, password: HTMLInputElement) {
+        if (username.value.length == 0) {
+            alert("请输入账号");
+        } else if (password.value.length == 0) {
+            alert("请输入密码");
+        } else {
+            let userinfo: string = '用户名：' + username.value + '密码：' + password.value;
+            alert(userinfo);
+            this.navCtrl.push(TabsPage)
+        }
     }
 
 }
